e4171ed7a754bd60f47239bef1e5cf607546bb64
geleasure <grleasure@gmail.com>
2021-08-15 15:35:41 -0400

    added all js and added license

--- index.js
+++ index.js
@@ -51,3 +51,302 @@ const questions = [
             'AngularJS',
         ]
     },
+     // Installation Instructions
+     {
+        type: 'confirm',
+        name: 'confirmInstallation',
+        message: 'Would you like to enter some information about installation?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'aboutInstallation',
+        message: 'Provide some information about installation:',
+        validate: installationInput => {
+            if(installationInput) {
+                return true;
+            } else {
+                console.log('Please enter about Installation!')
+                return false;
+            }
+        },
+        when: ({confirmInstallation}) => {
+            if (confirmInstallation) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    // Installation Commands 
+    {
+        type: 'confirm',
+        name: 'confirmInstallationCommands',
+        message: 'Would you like to add any commands for installation?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'aboutInstallationCommands',
+        message: 'Provide enter the command for the installation:',
+        validate: installationCommandInput => {
+            if(installationCommandInput) {
+                return true;
+            } else {
+                console.log('Please enter commands about the Installation!')
+                return false;
+            }
+        },
+        when: ({confirmInstallationCommands}) => {
+            if (confirmInstallationCommands) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    // Usage Instructions
+    {
+        type: 'confirm',
+        name: 'confirmUsage',
+        message: 'Would you like to enter some information about usage?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'aboutUsage',
+        message: 'Please provide some information about usage:',
+        validate: usageInput => {
+            if(usageInput) {
+                return true;
+            } else {
+                console.log('Please enter about Usage!')
+                return false;
+            }
+        },
+        when: ({confirmUsage}) => {
+            if (confirmUsage) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    // Usage Commands
+    {
+        type: 'confirm',
+        name: 'confirmUsageCommands',
+        message: 'Would you like to add any commands for usage?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'aboutUsageCommands',
+        message: 'Provide enter the command for the usage:',
+        validate: usageCommandInput => {
+            if(usageCommandInput) {
+                return true;
+            } else {
+                console.log('Please enter commands about the Usage!')
+                return false;
+            }
+        },
+        when: ({confirmUsageCommands}) => {
+            if (confirmUsageCommands) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    // License Selection
+    {
+        type: 'confirm',
+        name: 'confirmLicense',
+        message: 'Would you like to include a license?',
+        default: true
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select one of the license:',
+        choices: [{
+            name: 'GNU AGPLv3',
+            value: 'AGPL-3.0'
+        },
+        {
+            name: 'GNU GPLv3',
+            value: 'GPL-3.0'
+        },
+        {
+            name: 'GNU LGPLv3',
+            value: 'LGPL-3.0'
+        },
+        {
+            name: 'Mozilla Public License 2.0',
+            value: 'MPL-2.0'
+        },
+        {
+            name: 'Apache License 2.0',
+            value: 'Apache-2.0'
+        },
+        {
+            name: 'MIT License',
+            value: 'MIT'
+        },
+        {
+            name: 'Boost Software License 1.0',
+            value: 'BSL-1.0' 
+        },
+        {
+            name: 'The Unlicense',
+            value: 'Unlicense'
+        }],        
+        when: ({confirmLicense}) => {
+            if (confirmLicense) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    // Contributing Instructions
+    {
+        type: 'confirm',
+        name: 'confirmContributing',
+        message: 'Would you like to enter some information about contributing?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'aboutContributing',
+        message: 'Please enter details about contributing:',
+        default: 'Contributions, issues and feature requests are welcome. Feel free to check issues page if you want to contribute.',
+        when: ({confirmContributing}) => {
+            if (confirmContributing) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    // Tests Instructions
+    {
+        type: 'confirm',
+        name: 'confirmTests',
+        message: 'Would you like to enter some information about tests?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'aboutTests',
+        message: 'Provide some information about tests:',
+        validate: testsInput => {
+            if(testsInput) {
+                return true;
+            } else {
+                console.log('Please enter about tests!')
+                return false;
+            }
+        },
+        when: ({confirmTests}) => {
+            if (confirmTests) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    // Questions and Contact Information
+    {
+        type: 'confirm',
+        name: 'confirmQuestions',
+        message: 'Would you like to enter your contact information for questions?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: emailInput => {
+            if(emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address')
+                return false;
+            }
+        },
+        when: ({confirmQuestions}) => {
+            if (confirmQuestions) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your github Username?',
+        validate: githubInput => {
+            if(githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your github Username')
+                return false;
+            }
+        },
+        when: ({confirmQuestions}) => {
+            if (confirmQuestions) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile('./dist/' + fileName + '.md', data, err => {
+        if (err) {
+            reject(err);
+            return;
+        }
+
+        console.log(`
+
+╔═╗╦ ╦╔═╗╔═╗╔═╗╔═╗╔═╗┬
+╚═╗║ ║║  ║  ║╣ ╚═╗╚═╗│
+╚═╝╚═╝╚═╝╚═╝╚═╝╚═╝╚═╝o
+
+A README.md has been successfully generated. Please check the dist folder for the output!`)
+    });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    return inquirer
+    .prompt(questions);
+}
+
+// Function call to initialize app
+init()
+    .then(answers => {
+        return generateMarkdown(answers);
+    })
+    .then(content => {
+        return writeToFile('README', content)  
+    })
+    .catch(err => {
+        console.log(err);
+    });
